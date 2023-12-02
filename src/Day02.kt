59b596234683111f5bc1e1ea49ba811b2d1578fa
dhiaspaner <78903066+dhiaspaner@users.noreply.github.com>
2023-12-02 18:11:09 +0100

    refactor the main entry point

--- src/Day02.kt
+++ src/Day02.kt
@@ -83,15 +83,17 @@ class Day02 {
 fun main() {
     fun part1(input: List<String>): Int {
         val day02 = Day02()
-        return day02.solvePart2(input)
+        return day02.solvePart1(input)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val day02 = Day02()
+        return day02.solvePart2(input)
     }
 
     // test if implementation meets criteria from the description, like:
 
-    val input = readInput("Day02_test")
+    val input = readInput("Day02")
     part1(input).println()
+    part2(input).println()
 }
